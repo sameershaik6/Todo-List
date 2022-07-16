d8fb68c9ee2a53dbc1de8147411e0e59c38de8f2
sameer <sameerthsk@gmail.com>
2022-07-16 13:37:31 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react'
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+
+  const inputTask = useRef(null);
+
+  const addTask = () => {
+    if(currentTask === "") {
+      alert("please enter todo to display")
+    } else {
+      setTodoList([...todoList, currentTask]);
+      inputTask.current.value = "";
+      setCurrentTask("");
+    }
+  };
+
+  const deleteTask = (taskToDelete) => {
+      setTodoList(
+        todoList.filter((task) => {
+          return task !== taskToDelete;
+        })
+      )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>React To-Do List</h1>
+      <div className="head">
+        <input className='task' ref={inputTask} type="text" placeholder="Task..." onChange={(event) => {
+          setCurrentTask(event.target.value);
+         }} 
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <hr/>
+      {
+        todoList.length > 0 ? (
+          <ul>
+            {todoList.map((val, key) => {
+                return(
+                  <div id="task">
+                    <li key={key}>
+                      <input className='checkb' type="checkbox" />
+                      <span>{val}</span>
+                    </li>
+                    <button onClick={() => deleteTask(val)}>X</button>
+                  </div>
+                )
+              }) 
+            }
+          </ul>
+        ) : (
+          <div style={{color: "red"}}>
+            No todo's to display
+          </div>
+        )
+      }
+          </div>
   );
 }
 
